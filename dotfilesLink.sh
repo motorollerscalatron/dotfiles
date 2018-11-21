ddf1407aa87358b7b4af3942daa0e015db197827
motoroller.scalatron <motoroller.scalatron75@gmail.com>
2018-11-22 02:24:21 +0900

    link

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.rubocop.yml ~/.rubocop.yml
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+#ln -sf ~/dotfiles/.vimrc ~/.vimrc
+#ln -sf ~/dotfiles/.rubocop.yml ~/.rubocop.yml
+#ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+#mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+#cd ~/.vim/bundle && \
+#    git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+#git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+#git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
+
