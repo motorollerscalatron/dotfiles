cae1f89a82ffef75408f3f3c1864e81b067ea5fe
Masahiro Iwasaki <motoroller.scalatron75@gmail.com>
2020-01-16 02:11:29 +0900

    brew install node

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,6 +7,7 @@
 #brew install ag
 #brew install neovim
 #brew install awscli
+#brew install node
 #ln -sf ~/dotfiles/.vimrc ~/.vimrc
 #ln -sf ~/dotfiles/.rubocop.yml ~/.rubocop.yml
 #ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
