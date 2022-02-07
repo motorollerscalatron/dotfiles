7f45c78aa446d1044ab649bde460eca9ae1e0c9e
Masahiro Iwasaki <motoroller.scalatron75@gmail.com>
2022-02-07 16:22:25 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -18,13 +18,13 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.rubocop.yml ~/.rubocop.yml
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-cd ~/.vim/bundle && \
+mkdir -p ~/.config/nvim/autoload ~/.config/nvim/bundle && \
+curl -LSso ~/.config/nvim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone http://github.com/gmarik/vundle.git ~/.config/nvim/bundle/vundle
+cd ~/.config/nvim/bundle && \
     git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
-git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
-git clone https://github.com/mxw/vim-jsx.git ~/.vim/bundle/vim-jsx
+git clone https://github.com/pangloss/vim-javascript.git ~/.config/nvim/bundle/vim-javascript
+git clone https://github.com/mxw/vim-jsx.git ~/.config/nvim/bundle/vim-jsx
 git clone --recursive https://github.com/joel-porquet/zsh-dircolors-solarized ~/.zsh/zsh-dircolors-solarized
 
 cd ~/.zsh
